acbdbc468e66de816e834c9a5c7278a9c3786822
littleblack111 <littleblack11111@gmail.com>
2024-01-01 21:05:05 +0800

    Initial Commit

---
+++ dynamicMenu/dynamicMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  dynamicMenuApp.swift
+//  dynamicMenu
+//
+//  Created by system on 1/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct dynamicMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
